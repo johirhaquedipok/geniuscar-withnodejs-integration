5363a0d057c5ad19e5bb1a18908330c3cf7c8329
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-18 14:15:14 +0600

    input field to submit in db

--- src/AddServices/AddServices.js
+++ src/AddServices/AddServices.js
@@ -1,14 +1,44 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 const AddServices = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
-    </form>
+    <Container>
+      <Row>
+        <Col md={8} className="text-center">
+          <h3>Add some data to the form</h3>
+          <form
+            className="d-flex flex-column"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              className="mb-2"
+              placeholder="name"
+              {...register("firstName", { required: true, maxLength: 20 })}
+            />
+            <input
+              className="mb-2"
+              placeholder="description"
+              {...register("description")}
+            />
+            <input
+              className="mb-2"
+              placeholder="price"
+              type="number"
+              {...register("price")}
+            />
+            <input
+              className="mb-2"
+              placeholder="photo Url"
+              type="text"
+              {...register("img")}
+            />
+            <input className="mb-2" type="submit" />
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
